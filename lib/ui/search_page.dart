0a78b35b0add182bcfc7a6e5f597424951b40657
gnendraaw <itsgnendraaw@gmail.com>
2022-10-24 23:49:56 +0800

    add suggestion list on search feature

--- lib/ui/search_page.dart
+++ lib/ui/search_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:restaurant_app/common/style.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
+import 'package:restaurant_app/data/model/restaurant_search.dart';
 import 'package:restaurant_app/widgets/restaurant_search_card.dart';
-import 'package:restaurant_app/provider/restaurant_search_provider.dart';
 
 class SearchPage extends SearchDelegate {
   dynamic result;
+  late Future<RestaurantSearchResult> _restaurantResult;
 
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -35,39 +35,50 @@ class SearchPage extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    if (query.isEmpty) {
-      return const Center(child: Text("Let's find something!"));
-    }
-    return ChangeNotifierProvider<RestaurantSearchProvider>(
-      create: (_) =>
-          RestaurantSearchProvider(apiService: ApiService(), query: query),
-      child: Consumer<RestaurantSearchProvider>(
-        builder: (context, state, _) {
-          if (state.state == ResultState.loading) {
-            return const Center(
-                child: CircularProgressIndicator(color: secondaryColor));
-          } else if (state.state == ResultState.noData) {
-            return Center(child: Text(state.message));
-          } else if (state.state == ResultState.hasData) {
-            return ListView.builder(
-              itemCount: state.restaurantResult.restaurants.length,
-              itemBuilder: (context, index) {
-                var restaurant = state.restaurantResult.restaurants[index];
-                return RestaurantCard(restaurant: restaurant);
-              },
-            );
-          } else if (state.state == ResultState.error) {
-            return Center(child: Text(state.message));
-          } else {
-            return const Center();
-          }
-        },
-      ),
-    );
+    return _buildSearchList();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    if (query.isNotEmpty) {
+      _restaurantResult = ApiService().restaurantSearch(query);
+      return _buildSearchList();
+    }
     return const Center(child: Text("Let's find something"));
+  }
+
+  FutureBuilder<RestaurantSearchResult> _buildSearchList() {
+    return FutureBuilder(
+      future: _restaurantResult,
+      builder: (context, snapshot) {
+        var state = snapshot.connectionState;
+        if (state != ConnectionState.done) {
+          return const Center(
+              child: CircularProgressIndicator(color: secondaryColor));
+        } else {
+          if (snapshot.hasData) {
+            int? founded = snapshot.data?.founded;
+            if (founded! <= 0) {
+              return const Center(
+                  child: Text('Nothing found, try another keyword'));
+            }
+            return ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: snapshot.data?.restaurants.length,
+              itemBuilder: (context, index) {
+                var restaurant = snapshot.data?.restaurants[index];
+                return RestaurantCard(
+                  restaurant: restaurant!,
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          } else {
+            return const Center();
+          }
+        }
+      },
+    );
   }
 }
